82196d62cfe52efed6bdece13f4e54cc7d0e75d1
MaxHiit <pro.maxcode@gmail.com>
2022-05-06 00:41:43 +0200

    pages # create first component in chat room page | add animation on the form component

---
+++ src/views/Chatroom.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="container">
+		<Navbar />
+
+		<p>Chatroom</p>
+	</div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+import getUser from '@/composables/getUser';
+import { useRouter } from 'vue-router';
+import { watch } from 'vue';
+
+export default {
+	name: 'Chatroom',
+	components: { Navbar },
+	setup() {
+		const { user } = getUser();
+		const router = useRouter();
+
+		watch(user, () => {
+			console.log(user);
+			if (!user.value) {
+				router.push({ name: 'Welcome' });
+			}
+		});
+	}
+};
+</script>
+
+<style></style>
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,32 +1,57 @@
 <template>
 	<div class="container">
 		<h1>Welcome</h1>
-		<div class="flex flex-row gap gap-8">
-			<div class="w-6/12">
-				<img v-if="showLogin == false" alt="Vue logo" src="../assets/image-login.png" />
-				<div v-if="showLogin">
-					<h2>Login</h2>
-					<LoginForm />
-					<p>No account yet? <span @click="handleShowSignup">Sign Up</span> instead</p>
-				</div>
-			</div>
 
-			<div class="w-6/12">
-				<img v-if="showLogin" alt="Vue logo" src="../assets/image-login.png" />
-				<div v-else>
-					<h2>Sign Up</h2>
-					<SignupForm />
-					<p>Already registered? <span @click="handleShowSignup">Sign In</span> instead</p>
+		<div class="flex flex-row gap gap-8">
+			<transition
+				appear
+				@before-enter="beforeEnterOpacity"
+				@enter="enterOpacity"
+				@before-leave="beforeLeave"
+				@leave="leaveOpacity"
+				:css="false"
+			>
+				<div v-if="showLogin" class="w-6/12">
+					<h2>Login</h2>
+					<LoginForm @login="enterChat" />
+					<p>
+						No account yet?
+						<span @click="handleShowSignup" class="text-violet-700 cursor-pointer font-semibold"
+							>Sign Up</span
+						>
+						instead
+					</p>
 				</div>
-			</div>
+				<div v-else class="w-6/12">
+					<h2>Sign Up</h2>
+					<SignupForm @signup="enterChat" />
+					<p>
+						Already registered?
+						<span @click="handleShowSignup" class="text-violet-700 cursor-pointer font-semibold"
+							>Sign In</span
+						>
+						instead
+					</p>
+				</div>
+			</transition>
+			<transition appear @before-enter="beforeEnterScale" @enter="enterScale">
+				<template v-if="showLogin">
+					<img class="w-6/12" alt="Vue logo" src="../assets/image-login.png" />
+				</template>
+				<template v-else>
+					<img class="w-6/12" alt="Vue logo" src="../assets/image-login.png" />
+				</template>
+			</transition>
 		</div>
 	</div>
 </template>
 
 <script>
 import { ref } from 'vue';
+import gsap from 'gsap';
 import SignupForm from '@/components/SingupForm.vue';
 import LoginForm from '@/components/LoginForm.vue';
+import { useRouter } from 'vue-router';
 
 export default {
 	name: 'Welcome',
@@ -36,12 +61,66 @@ export default {
 	},
 	setup() {
 		const showLogin = ref(true);
+		const router = useRouter();
 
 		const handleShowSignup = () => {
 			showLogin.value = !showLogin.value;
 		};
 
-		return { showLogin, handleShowSignup };
+		const enterChat = () => {
+			router.push({ name: 'Chatroom' });
+		};
+
+		const beforeEnterScale = (el) => {
+			el.style.transform = 'scaleX(0)';
+		};
+
+		const enterScale = (el, done) => {
+			gsap.to(el, {
+				scaleX: 1,
+				transformOrigin: 'right center',
+				duration: 0.4,
+				onComplete: done
+			});
+		};
+
+		const beforeLeave = (el) => {
+			console.log('before leave - make transition');
+		};
+
+		const leave = (el) => {
+			gsap.to(el, {
+				scaleX: 0,
+				transformOrigin: 'left center',
+				duration: 0.4
+			});
+		};
+
+		const beforeEnterOpacity = (el) => {
+			el.style.opacity = 0;
+			el.style.transform = 'translateX(100px)';
+		};
+
+		const enterOpacity = (el, done) => {
+			gsap.to(el, {
+				opacity: 1,
+				x: 0,
+				duration: 0.4,
+				onComplete: done
+			});
+		};
+
+		return {
+			showLogin,
+			handleShowSignup,
+			enterChat,
+			beforeEnterScale,
+			enterScale,
+			beforeLeave,
+			leave,
+			beforeEnterOpacity,
+			enterOpacity
+		};
 	}
 };
 </script>
